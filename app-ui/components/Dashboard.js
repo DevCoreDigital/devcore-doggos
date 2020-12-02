4d857472831d3211ea73c53cfe99e884e1028f08
Mike Dombrowski <mike@acsbdevcore.com>
2020-12-02 16:22:57 -0500

    Added lambda to retrieve list of dogs

--- app-ui/components/Dashboard.js
+++ app-ui/components/Dashboard.js
@@ -1,10 +1,20 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
 import { API, Auth } from 'aws-amplify';
 import { API_NAME } from '@env';
 
 export default function Dashboard({}) {
   const [status, setStatus] = useState('Checking...');
+  const [doggos, setDoggos] = useState({});
+  const [doggosLoading, setDoggosLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   const getHealth = useCallback(async () => {
     await API.get(API_NAME, '/health', {})
@@ -17,6 +27,22 @@ export default function Dashboard({}) {
         console.log('error', error);
       });
   }, []);
+
+  const loadDoggos = async () => {
+    setDoggosLoading(true);
+    API.post(API_NAME, '/findDoggos', {})
+      .then((res) => {
+        const doggoList = JSON.parse(res.body);
+        setDoggos(doggoList);
+        return res;
+      })
+      .catch((err) => {
+        setStatus('Unhealthy');
+      })
+      .finally(() => {
+        setDoggosLoading(false);
+      });
+  };
 
   const signOut = useCallback(async () => {
     try {
@@ -34,6 +60,25 @@ export default function Dashboard({}) {
     <View style={styles.container}>
       <Text style={styles.title}>Dashboard</Text>
       <Text>Server Health: {status}</Text>
+      {doggosLoading ? (
+        <Text>Loading doggos...</Text>
+      ) : (
+        <TouchableOpacity onPress={loadDoggos}>
+          <Text>Get Doggos</Text>
+        </TouchableOpacity>
+      )}
+      {!!Object.keys(doggos).length && (
+        <FlatList
+          data={Object.keys(doggos)}
+          renderItem={({ item }) => {
+            return (
+              <Text style={styles.doggoName} key={doggos[item].animalId}>
+                {doggos[item].animalName}
+              </Text>
+            );
+          }}></FlatList>
+      )}
+
       <Button title="Sign Out" onPress={() => signOut()} />
     </View>
   );
@@ -57,5 +102,8 @@ const styles = StyleSheet.create({
     height: 40,
     borderColor: 'gray',
     borderWidth: 1
+  },
+  doggoName: {
+    color: 'white'
   }
 });
